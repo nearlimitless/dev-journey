d595b32e0f6082bb9b622999cf7ad6e8f156fa93
nearlimitless <yourbarondedios@gmail.com>
2026-02-23 22:18:53 +0800

    Implemented a *not working yet* interactive terminal

--- learning-react/learning-react-project-one/src/components/Terminal.jsx
+++ learning-react/learning-react-project-one/src/components/Terminal.jsx
@@ -1,8 +1,26 @@
+import {useState} from 'react';
+
+
 function Terminal () {
+
+const [input, setInput] = useState("");
+
     return (
         <>
             <div className="terminal-container">
                 <p>Terminal (in progress)</p>
+                <input
+                    type="text"
+                    value={input}
+                    onChange={e=>setInput(e.target.value)}
+                    onKeyDown={e=> {
+                        if (e.key === "Enter") {
+                            window.alert('Test')
+                        }
+                    }}
+                >
+                
+                </input>
             </div>
         </>
     );
